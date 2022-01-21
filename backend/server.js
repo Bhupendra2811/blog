eb0f7adaf6a47fb877e67f4847b55cadf1c23b19
bsr9099 <76525392+bsr9099@users.noreply.github.com>
2022-01-21 20:31:52 +0530

    g

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,11 @@ app.use("/api/blog/",blogRoutes)
 
 if(process.env.NODE_ENV =='production')
 {
-    app.use('/',express.static('frontend/build'))
+    app.use('/',express.static('client/build'))
 
     app.get('*',(req,res)=>{
 
-        res.sendFile(path.resolve(__dirname,'frontend/build/index.html'))
+        res.sendFile(path.resolve(__dirname,'client/build/index.html'))
     })
 }
 
